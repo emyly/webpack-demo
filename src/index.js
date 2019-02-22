f7e5107979d9e3cf3530427a147ba5418c44dc5d
jianfen shen <shenjf@broada.com>
2019-02-22 17:05:35 +0800

    第二天

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import _ from "lodash";
-import "./style.css";
-import Jpg from "./WechatIMG105.jpg";
-import Data from "./data.xml";
+import printMe from "./print";
 function component() {
   var element = document.createElement("div");
+  var button = document.createElement("button");
 
   element.innerHTML = _.join(["Hello1", "webpack"], " ");
-  element.className += "hello";
-  var myIcon = new Image();
-  myIcon.src = Jpg;
-  element.appendChild(myIcon);
-  console.log(Data);
+  button.innerHTML = "click me";
+  button.onclick = printMe;
+  element.appendChild(button);
   return element;
 }
 
